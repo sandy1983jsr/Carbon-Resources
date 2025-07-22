6658061b4aa8c43a987aad3c123c4b032296f2b1
sandy1983jsr <sandeep.uce208@gmail.com>
2025-07-22 23:47:35 +0530

    Update baseline_assessment.py

--- modules/baseline_assessment.py
+++ modules/baseline_assessment.py
@@ -4,8 +4,15 @@ class BaselineAssessment:
         self.results = {}
 
     def run_assessment(self):
-        self.results['energy_total'] = self.datasets['energy_consumption']['kwh_consumed'].sum()
-        input_tons = self.datasets['material_input']['quantity_tons'].sum()
-        output_tons = self.datasets['material_output']['quantity_tons'].sum()
-        self.results['material_yield'] = output_tons / input_tons if input_tons else None
-        return self.results
+        res = {}
+        # Energy
+        if "energy_consumption" in self.datasets:
+            res["energy_total"] = self.datasets["energy_consumption"]["kwh_consumed"].sum()
+        # Material yield
+        if "material_input" in self.datasets and "material_output" in self.datasets:
+            input_tons = self.datasets["material_input"]["quantity_tons"].sum()
+            output_tons = self.datasets["material_output"]["quantity_tons"].sum()
+            res["material_yield"] = output_tons / input_tons if input_tons else None
+            res["material_loss"] = input_tons - output_tons
+        self.results = res
+        return res
