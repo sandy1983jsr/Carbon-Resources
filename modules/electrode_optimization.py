865a3bee56d35150fa0c2912ae1268a19e54f3bb
sandy1983jsr <sandeep.uce208@gmail.com>
2025-07-22 23:39:19 +0530

    Update electrode_optimization.py

--- modules/electrode_optimization.py
+++ modules/electrode_optimization.py
@@ -1,1 +1,17 @@
+class ElectrodeOptimization:
+    def __init__(self, datasets):
+        self.datasets = datasets
+        self.results = {}
 
+    def optimize(self):
+        df = self.datasets.get('furnace_data')
+        results = {}
+        if df is not None and 'electrode_paste_consumption_kg' in df.columns and 'production_tons' in df.columns:
+            total_paste = df['electrode_paste_consumption_kg'].sum()
+            total_prod = df['production_tons'].sum()
+            results['paste_consumption'] = {
+                'total_paste_kg': total_paste,
+                'specific_consumption': total_paste / total_prod if total_prod else None
+            }
+        self.results = results
+        return results
