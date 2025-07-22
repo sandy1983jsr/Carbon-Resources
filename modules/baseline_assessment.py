a52ac13b6821135c42395e190d33ab489d01cab9
sandy1983jsr <sandeep.uce208@gmail.com>
2025-07-22 23:52:52 +0530

    Update baseline_assessment.py

--- modules/baseline_assessment.py
+++ modules/baseline_assessment.py
@@ -5,14 +5,32 @@ class BaselineAssessment:
 
     def run_assessment(self):
         res = {}
-        # Energy
+
+        # Energy consumption total
         if "energy_consumption" in self.datasets:
-            res["energy_total"] = self.datasets["energy_consumption"]["kwh_consumed"].sum()
-        # Material yield
+            ec = self.datasets["energy_consumption"]
+            if "kwh_consumed" in ec.columns:
+                res["energy_total"] = ec["kwh_consumed"].sum()
+            else:
+                res["energy_total"] = None
+        else:
+            res["energy_total"] = None
+
+        # Material yield and loss
         if "material_input" in self.datasets and "material_output" in self.datasets:
-            input_tons = self.datasets["material_input"]["quantity_tons"].sum()
-            output_tons = self.datasets["material_output"]["quantity_tons"].sum()
-            res["material_yield"] = output_tons / input_tons if input_tons else None
-            res["material_loss"] = input_tons - output_tons
+            mi = self.datasets["material_input"]
+            mo = self.datasets["material_output"]
+            input_tons = mi["quantity_tons"].sum() if "quantity_tons" in mi.columns else None
+            output_tons = mo["quantity_tons"].sum() if "quantity_tons" in mo.columns else None
+            if input_tons and output_tons and input_tons != 0:
+                res["material_yield"] = output_tons / input_tons
+                res["material_loss"] = input_tons - output_tons
+            else:
+                res["material_yield"] = None
+                res["material_loss"] = None
+        else:
+            res["material_yield"] = None
+            res["material_loss"] = None
+
         self.results = res
         return res
